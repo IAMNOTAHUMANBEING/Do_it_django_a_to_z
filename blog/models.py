01e0a93828978c58c6feac3b3ec5f697529a5e54
HyunBin Lee <gusqls9879@naver.com>
2021-12-23 16:06:44 +0900

    포스트 상세 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30)     # 글씨 제한이 있는 문자 필드
     content = models.TextField()     # 길이제한이 없는 텍스트 필드 
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)     # 월,일,시,분,초 까지 작성일 기록 필드
     updated_at = models.DateTimeField(auto_now=True)
     # author: 추후 작성 예정
