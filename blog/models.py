f945ca8dd329d30dd5aa22735054c586890a2e2c
HyunBin Lee <gusqls9879@naver.com>
2021-12-24 18:16:49 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -2,28 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)    # 사람이 읽을 수 있는 텍스트로 고유 URL을 만듬
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
-    title = models.CharField(max_length=30)     # 글씨 제한이 있는 문자 필드
-    hook_text = models.CharField(max_length=100, blank=True) # 미리보기 내용
-    content = models.TextField()     # 길이제한이 없는 텍스트 필드
+    title = models.CharField(max_length=30)  # 글씨 제한이 있는 문자 필드
+    hook_text = models.CharField(max_length=100, blank=True)  # 미리보기 내용
+    content = models.TextField()  # 길이제한이 없는 텍스트 필드
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)     # 월,일,시,분,초 까지 작성일 기록 필드
+    created_at = models.DateTimeField(auto_now_add=True)  # 월,일,시,분,초 까지 작성일 기록 필드
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)  # 사용자가 삭제되면 작성자 Null로 변환
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)  # 사용자가 삭제되면 작성자 null로 변환
     # author = models.ForeignKey(User, on_delete=models.CASCADE)  # 사용자가 삭제되면 작성한 포스트도 삭제
 
-    def __str__(self):  
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)    # black와 null 구분
+
+    def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
-    def get_absolute_url(self):     # 페이지의 주소를 가져오는 함수
+    def get_absolute_url(self):  # 페이지의 주소를 가져오는 함수
         return f'/blog/{self.pk}/'
 
-    def get_file_name(self):    # 업로드 파일 이름 가져오는 함수
+    def get_file_name(self):  # 업로드 파일 이름 가져오는 함수
         return os.path.basename(self.file_upload.name)
 
-    def get_file_ext(self):     # 파일 이름에서 확장자만 리턴하는 함수
+    def get_file_ext(self):  # 파일 이름에서 확장자만 리턴하는 함수
         return self.get_file_name().split('.')[-1]
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'    # 최신순 정렬
+
+    def get_context_data(self, **kwargs): # ListView에서 내장하고 있는 메서드 model = post 하면 post_list = Post.objects.all()을 자동으로 명령 하는 등의 기능을 함, 오버라이딩 해서 정보 추가
+        context = super(PostList, self).get_context_data()     # 기존 get_context_data가 제공하던 기능 저장
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()      # 카테고리가 지정되지 않은 포스트 개수를 세서 쿼리셋으로 만들어 저장
+
+        return context
 
 class PostDetail(DetailView):
     model = Post
